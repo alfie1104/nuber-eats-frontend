d9992358ecefbe25bb8d9db09f847a878d235f87
peter_alfie <alfie1104@gmail.com>
2021-02-17 08:35:43 +0900

    #17.5 Search Part is done

---
+++ src/fragments.ts
@@ -0,0 +1,22 @@
+import { gql } from "@apollo/client";
+
+export const RESTAURANT_FRAGMENT = gql`
+  fragment RestaurantParts on Restaurant {
+    id
+    name
+    coverImg
+    category {
+      name
+    }
+    address
+    isPromoted
+  }
+`;
+
+//여러곳에서 반복 활용되는 type을 Fragment로 설정하고 불러와서 사용가능
+/*
+  사용하는 곳에서
+  ...RestaurantParts 라고 GraphQL 구문안에 적고
+
+  구문이 끝나는 부분에서 ${RESTAURANT_FRAGMENT}로 import시킴
+*/
--- src/pages/client/search.tsx
+++ src/pages/client/search.tsx
@@ -1,11 +1,61 @@
 import React, { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
+import { gql, useLazyQuery } from "@apollo/client";
+import { RESTAURANT_FRAGMENT } from "../../fragments";
+import {
+  searchRestaurant,
+  searchRestaurantVariables,
+} from "../../__generated__/searchRestaurant";
+
+const SEARCH_RESTAURANT = gql`
+  query searchRestaurant($input: SearchRestaurantInput!) {
+    searchRestaurant(input: $input) {
+      ok
+      error
+      totalPages
+      totalResults
+      restaurants {
+        ...RestaurantParts
+      }
+    }
+  }
+  ${RESTAURANT_FRAGMENT}
+`;
 
 export const Search = () => {
   const location = useLocation();
-  useEffect(() => {
-    console.log(location);
-  }, []);
+  const history = useHistory();
+  const [callQuery, { loading, data, called }] = useLazyQuery<
+    searchRestaurant,
+    searchRestaurantVariables
+  >(SEARCH_RESTAURANT);
 
-  return <h1>Search Page</h1>;
+  useEffect(() => {
+    const [_, query] = location.search.split("?term=");
+    if (!query) {
+      //history.push("/");
+      //push와 다르게 replace는 history에 기록을 남기지 않음. 현재 페이지를 그냥 새로운 페이지로 대체해버림
+      return history.replace("/");
+    }
+    callQuery({
+      variables: {
+        input: {
+          page: 1,
+          query,
+        },
+      },
+    });
+  }, [history, location]);
+
+  console.log(loading, data, called);
+
+  return (
+    <div>
+      <Helmet>
+        <title>Search | Nuber Eats</title>
+      </Helmet>
+      <h1>Search Page</h1>
+    </div>
+  );
 };
