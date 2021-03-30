e58d811e988a7612a53571c6a46342ec72cc6991
peter_alfie <alfie1104@gmail.com>
2021-03-31 08:49:06 +0900

    #23.2 subscribeToMore is done

--- src/apollo.ts
+++ src/apollo.ts
@@ -3,8 +3,11 @@ import {
   createHttpLink,
   InMemoryCache,
   makeVar,
+  split,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 
 import { LOCALSTORAGE_TOKEN } from "./constants";
 
@@ -12,6 +15,16 @@ const token = localStorage.getItem(LOCALSTORAGE_TOKEN);
 
 export const isLoggedInVar = makeVar(Boolean(token));
 export const authTokenVar = makeVar(token);
+
+const wsLink = new WebSocketLink({
+  uri: "ws://localhost:4000/graphql",
+  options: {
+    reconnect: true,
+    connectionParams: {
+      "x-jwt": authTokenVar() || "",
+    },
+  },
+});
 
 /*
   local state를 사용하기 위해 typePolicies옵션 정의.
@@ -35,12 +48,33 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+// The split function takes three parameters:
+//
+// * A function that's called for each operation to execute
+// * The Link to use for an operation if the function returns a "truthy" value
+// * The Link to use for an operation if the function returns a "falsy" value
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    /* 
+    상황에 따라서 적절한 Link (Websocket 혹은 http)를 연결시켜주도록 설정 
+    아래 return값이 true 일 경우, wsLink를 사용하게 되고
+    false일 경우 authLink를 사용하게 됨
+    */
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
 /*
   여러 Link를 동시에 사용하기 위해서
   하나의 Link에서 concat함수를 호출하면서 인자로 다른 Link를 넣으면 됨
 */
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: splitLink,
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -1,8 +1,23 @@
-import { useQuery } from "@apollo/client";
+import { useQuery, useSubscription } from "@apollo/client";
 import gql from "graphql-tag";
-import React from "react";
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet-async";
 import { useParams } from "react-router";
+import { FULL_ORDER_FRAGMENT } from "../fragments";
 import { getOrder, getOrderVariables } from "../__generated__/getOrder";
+import {
+  orderUpdates,
+  orderUpdatesVariables,
+} from "../__generated__/orderUpdates";
+
+const ORDER_SUBSCRIPTION = gql`
+  subscription orderUpdates($input: OrderUpdatesInput!) {
+    orderUpdates(input: $input) {
+      ...FullOrderParts
+    }
+  }
+  ${FULL_ORDER_FRAGMENT}
+`;
 
 const GET_ORDER = gql`
   query getOrder($input: GetOrderInput!) {
@@ -10,21 +25,11 @@ const GET_ORDER = gql`
       ok
       error
       order {
-        id
-        status
-        total
-        driver {
-          email
-        }
-        customer {
-          email
-        }
-        restaurant {
-          name
-        }
+        ...FullOrderParts
       }
     }
   }
+  ${FULL_ORDER_FRAGMENT}
 `;
 
 interface IParams {
@@ -33,7 +38,55 @@ interface IParams {
 
 export const Order = () => {
   const params = useParams<IParams>();
-  const { data } = useQuery<getOrder, getOrderVariables>(GET_ORDER, {
+  const { data, subscribeToMore } = useQuery<getOrder, getOrderVariables>(
+    GET_ORDER,
+    {
+      variables: {
+        input: {
+          id: +params.id,
+        },
+      },
+    }
+  );
+
+  useEffect(() => {
+    if (data?.getOrder.ok) {
+      subscribeToMore({
+        document: ORDER_SUBSCRIPTION,
+        variables: {
+          input: {
+            id: +params.id,
+          },
+        },
+        updateQuery: (
+          prev,
+          {
+            subscriptionData: { data },
+          }: { subscriptionData: { data: orderUpdates } }
+        ) => {
+          if (!data) {
+            return prev;
+          }
+
+          //updateQuery의 return값은 기존에 가지고 있던 query의 결과 값과 동일한 구조를 가져야함
+
+          return {
+            getOrder: {
+              ...prev.getOrder,
+              order: {
+                ...data.orderUpdates,
+              },
+            },
+          };
+        },
+      });
+    }
+  }, [data]);
+
+  const { data: subscriptionData } = useSubscription<
+    orderUpdates,
+    orderUpdatesVariables
+  >(ORDER_SUBSCRIPTION, {
     variables: {
       input: {
         id: +params.id,
@@ -41,7 +94,44 @@ export const Order = () => {
     },
   });
 
-  console.log(data);
+  console.log(subscriptionData);
 
-  return <h1>{params.id}</h1>;
+  return (
+    <div className="mt-32 container flex justify-center">
+      <Helmet>
+        <title>Order #{params.id} | Nuber Eats</title>
+      </Helmet>
+      <div className="border border-gray-800 w-full max-w-screen-sm flex flex-col justify-center">
+        <h4 className="bg-gray-800 w-full py-5 text-white text-center text-xl">
+          Order #{params.id}
+        </h4>
+        <h5 className="p-5 pt-10 text-3xl text-center ">
+          ${data?.getOrder.order?.total}
+        </h5>
+        <div className="p-5 text-xl grid gap-6">
+          <div className="border-t pt-5 border-gray-700">
+            Prepared By:{" "}
+            <span className="font-medium">
+              {data?.getOrder.order?.restaurant?.name}
+            </span>
+          </div>
+          <div className="border-t pt-5 border-gray-700 ">
+            Deliver To:{" "}
+            <span className="font-medium">
+              {data?.getOrder.order?.customer?.email}
+            </span>
+          </div>
+          <div className="border-t border-b py-5 border-gray-700">
+            Driver:{" "}
+            <span className="font-medium">
+              {data?.getOrder.order?.driver?.email || "Not yet."}
+            </span>
+          </div>
+          <span className=" text-center mt-5 mb-3  text-2xl text-lime-600">
+            Status: {data?.getOrder.order?.status}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
