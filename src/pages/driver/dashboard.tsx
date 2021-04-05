018c4d10c985e53523682f9ac69b74fd304060b9
peter_alfie <alfie1104@gmail.com>
2021-04-06 08:38:38 +0900

    #23.7 Driver Dashboard part three is done

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,12 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GoogleMapReact from "google-map-react";
 
+interface ICoords {
+  lat: number;
+  lng: number;
+}
+
 export const Dashboard = () => {
+  const [driverCoords, setDriverCoords] = useState<ICoords>({ lat: 0, lng: 0 });
+  const [map, setMap] = useState<any>();
+  const [maps, setMaps] = useState<any>();
+
+  const onSuccess = ({
+    coords: { latitude, longitude },
+  }: GeolocationPosition) => {
+    setDriverCoords({ lat: latitude, lng: longitude });
+  };
+
+  const onError = (error: GeolocationPositionError) => {
+    console.log(error);
+  };
+
+  useEffect(() => {
+    navigator.geolocation.watchPosition(onSuccess, onError, {
+      enableHighAccuracy: true,
+    });
+  }, []);
+
+  useEffect(() => {
+    if (map && maps) {
+      map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    }
+  }, [driverCoords.lat, driverCoords.lng]);
+
+  const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
+    map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    setMap(map);
+    setMaps(maps);
+  };
+
   return (
     <div>
-      <div className="py-52 bg-gray-800 ">
-        <GoogleMapReact bootstrapURLKeys={{ key: "" }}>
-          <h1>hello</h1>
+      <div
+        className="overflow-hidden"
+        style={{ width: window.innerWidth, height: "50vh" }}
+      >
+        <GoogleMapReact
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={onApiLoaded}
+          defaultZoom={16}
+          draggable={false}
+          defaultCenter={{ lat: 37.58, lng: 126.95 }}
+          bootstrapURLKeys={{ key: "" }}
+        >
+          <div
+            // @ts-ignore
+            lat={driverCoords.lat}
+            lng={driverCoords.lng}
+            className="text-lg"
+          >
+            🚕
+          </div>
         </GoogleMapReact>
       </div>
     </div>
