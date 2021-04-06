e3df20e390e0d18058a006f31d1549e7b1fd5a06
peter_alfie <alfie1104@gmail.com>
2021-04-07 08:38:22 +0900

    #23.8 Address Geocoding is done

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -6,9 +6,17 @@ interface ICoords {
   lng: number;
 }
 
+interface IDriverProps {
+  lat: number;
+  lng: number;
+  $hover?: any;
+}
+
+const Driver: React.FC<IDriverProps> = () => <div className="text-lg">🚕</div>;
+
 export const Dashboard = () => {
   const [driverCoords, setDriverCoords] = useState<ICoords>({ lat: 0, lng: 0 });
-  const [map, setMap] = useState<any>();
+  const [map, setMap] = useState<google.maps.Map>();
   const [maps, setMaps] = useState<any>();
 
   const onSuccess = ({
@@ -28,13 +36,22 @@ export const Dashboard = () => {
   }, []);
 
   useEffect(() => {
-    if (map && maps) {
-      map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    if (map) {
+      map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
+      const geocoder = new google.maps.Geocoder();
+      geocoder.geocode(
+        {
+          location: new google.maps.LatLng(driverCoords.lat, driverCoords.lng),
+        },
+        (results, status) => {
+          console.log(status, results);
+        }
+      );
     }
   }, [driverCoords.lat, driverCoords.lng]);
 
-  const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
-    map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+  const onApiLoaded = ({ map, maps }: { map: google.maps.Map; maps: any }) => {
+    map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
     setMap(map);
     setMaps(maps);
   };
@@ -53,14 +70,7 @@ export const Dashboard = () => {
           defaultCenter={{ lat: 37.58, lng: 126.95 }}
           bootstrapURLKeys={{ key: "" }}
         >
-          <div
-            // @ts-ignore
-            lat={driverCoords.lat}
-            lng={driverCoords.lng}
-            className="text-lg"
-          >
-            🚕
-          </div>
+          <Driver lat={driverCoords.lat} lng={driverCoords.lng} />
         </GoogleMapReact>
       </div>
     </div>
